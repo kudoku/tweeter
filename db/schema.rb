9f5b9bb37bf3d6315510edf38be939dcda41bacc
JPark <park.joonw@gmail.com>
2015-01-20 13:49:40 -0700

    heroku/postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150120195853) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tweets", force: true do |t|
     t.string   "body"
     t.integer  "user_id"
@@ -35,7 +38,7 @@ ActiveRecord::Schema.define(version: 20150120195853) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
